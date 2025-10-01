4487b15bb5a034fa0e007c1a0c5695a92e3cc4ca
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-10-01 17:46:37 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	
+//Question 1
+public static boolean validIndex(int[] array, int integer) {
+	try {
+		int test = array[integer];
+		return (true); }
+	catch (ArrayIndexOutOfBoundsException e) {
+		return (false); }
+	}
+//Question 2
+public static int divide(int integer1, int integer2) {
+	try {
+		return(integer1 / integer2);
+	}
+	catch (ArithmeticException e) { 
+		return(0);
+	}
+}
+//Question 3
+public static int safeConvertStringtoInt(String string1) {
+	try {
+		return(Integer.parseInt(string1));
+	}
+	catch (NumberFormatException e) {
+		return(0);
+	}
+}
 
 }
+
